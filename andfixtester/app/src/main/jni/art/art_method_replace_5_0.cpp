53ad565fe79df639f4ad03c7f6f8cb4304809841
“smartdone” <smartdone@outlook.com>
2017-09-03 07:55:05 +0800

    android 5.0 swap function

--- andfixtester/app/src/main/jni/art/art_method_replace_5_0.cpp
+++ andfixtester/app/src/main/jni/art/art_method_replace_5_0.cpp
@@ -50,28 +50,76 @@ void replace_5_0(JNIEnv* env, jobject src, jobject dest) {
 	art::mirror::ArtMethod* dmeth =
 			(art::mirror::ArtMethod*) env->FromReflectedMethod(dest);
 
-	reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->class_loader_ =
-			reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->class_loader_; //for plugin classloader
-	reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->clinit_thread_id_ =
-			reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->clinit_thread_id_;
-	reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->status_ = reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->status_-1;
-	//for reflection invoke
 	reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->super_class_ = 0;
+	reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->super_class_ = 0;
 
-	smeth->declaring_class_ = dmeth->declaring_class_;
-    smeth->access_flags_ = dmeth->access_flags_ | 0x0001;
-    smeth->dex_cache_resolved_types_ = dmeth->dex_cache_resolved_types_;
-    smeth->dex_cache_resolved_methods_ = dmeth->dex_cache_resolved_methods_;
-    smeth->dex_cache_strings_ = dmeth->dex_cache_strings_;
-    smeth->dex_code_item_offset_ = dmeth->dex_code_item_offset_;
-    smeth->dex_method_index_ = dmeth->dex_method_index_;
-    smeth->gc_map_ = dmeth->gc_map_;
-    smeth->entry_point_from_jni_ = dmeth->entry_point_from_jni_;
-    smeth->entry_point_from_quick_compiled_code_ = dmeth->entry_point_from_quick_compiled_code_;
+	reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->class_loader_ ^=
+			reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->class_loader_; //for plugin classloader
+	reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->clinit_thread_id_ ^=
+			reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->clinit_thread_id_;
+	reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->status_ ^=
+			reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->status_;
 
-    smeth->entry_point_from_interpreter_ = dmeth->entry_point_from_interpreter_;
+	smeth->declaring_class_ ^= dmeth->declaring_class_;
+	smeth->access_flags_ ^= dmeth->access_flags_ | 0x0001;
+	smeth->dex_cache_resolved_types_ ^= dmeth->dex_cache_resolved_types_;
+	smeth->dex_cache_resolved_methods_ ^= dmeth->dex_cache_resolved_methods_;
+	smeth->dex_cache_strings_ ^= dmeth->dex_cache_strings_;
+	smeth->dex_code_item_offset_ ^= dmeth->dex_code_item_offset_;
+	smeth->dex_method_index_ ^= dmeth->dex_method_index_;
+	smeth->gc_map_ ^= dmeth->gc_map_;
+	smeth->entry_point_from_jni_ ^= dmeth->entry_point_from_jni_;
+	smeth->entry_point_from_quick_compiled_code_ ^= dmeth->entry_point_from_quick_compiled_code_;
 
-    smeth->method_index_ = dmeth->method_index_;
+	smeth->entry_point_from_interpreter_ ^= dmeth->entry_point_from_interpreter_;
+
+	smeth->method_index_ ^= dmeth->method_index_;
+
+	reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->class_loader_ ^=
+			reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->class_loader_; //for plugin classloader
+	reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->clinit_thread_id_ ^=
+			reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->clinit_thread_id_;
+	reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->status_ ^= reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->status_-1;
+	//for reflection invoke
+
+	dmeth->declaring_class_ ^= smeth->declaring_class_;
+	dmeth->access_flags_ ^= smeth->access_flags_ | 0x0001;
+	dmeth->dex_cache_resolved_types_ ^= smeth->dex_cache_resolved_types_;
+	dmeth->dex_cache_resolved_methods_ ^= smeth->dex_cache_resolved_methods_;
+	dmeth->dex_cache_strings_ ^= smeth->dex_cache_strings_;
+	dmeth->dex_code_item_offset_ ^= smeth->dex_code_item_offset_;
+	dmeth->dex_method_index_ ^= smeth->dex_method_index_;
+	dmeth->gc_map_ ^= smeth->gc_map_;
+	dmeth->entry_point_from_jni_ ^= smeth->entry_point_from_jni_;
+	dmeth->entry_point_from_quick_compiled_code_ ^= smeth->entry_point_from_quick_compiled_code_;
+
+	dmeth->entry_point_from_interpreter_ ^= smeth->entry_point_from_interpreter_;
+
+	dmeth->method_index_ ^= smeth->method_index_;
+
+
+	reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->class_loader_ ^=
+			reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->class_loader_; //for plugin classloader
+	reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->clinit_thread_id_ ^=
+			reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->clinit_thread_id_;
+	reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->status_ ^=
+			reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->status_;
+
+	smeth->declaring_class_ ^= dmeth->declaring_class_;
+	smeth->access_flags_ ^= dmeth->access_flags_ | 0x0001;
+	smeth->dex_cache_resolved_types_ ^= dmeth->dex_cache_resolved_types_;
+	smeth->dex_cache_resolved_methods_ ^= dmeth->dex_cache_resolved_methods_;
+	smeth->dex_cache_strings_ ^= dmeth->dex_cache_strings_;
+	smeth->dex_code_item_offset_ ^= dmeth->dex_code_item_offset_;
+	smeth->dex_method_index_ ^= dmeth->dex_method_index_;
+	smeth->gc_map_ ^= dmeth->gc_map_;
+	smeth->entry_point_from_jni_ ^= dmeth->entry_point_from_jni_;
+	smeth->entry_point_from_quick_compiled_code_ ^= dmeth->entry_point_from_quick_compiled_code_;
+
+	smeth->entry_point_from_interpreter_ ^= dmeth->entry_point_from_interpreter_;
+
+	smeth->method_index_ ^= dmeth->method_index_;
+
 
 	LOGD("replace_5_0: %d , %d", smeth->entry_point_from_quick_compiled_code_,
 			dmeth->entry_point_from_quick_compiled_code_);

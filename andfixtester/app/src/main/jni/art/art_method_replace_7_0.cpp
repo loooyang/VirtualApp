adf0b91ba35a7cf4d104d2d02f584e5255762638
“smartdone” <smartdone@outlook.com>
2017-09-02 23:32:19 +0800

    android 7.0 swap function

--- andfixtester/app/src/main/jni/art/art_method_replace_7_0.cpp
+++ andfixtester/app/src/main/jni/art/art_method_replace_7_0.cpp
@@ -52,29 +52,66 @@ void replace_7_0(JNIEnv* env, jobject src, jobject dest) {
 
 //	reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->class_loader_ =
 //			reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->class_loader_; //for plugin classloader
-	reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->clinit_thread_id_ =
-			reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->clinit_thread_id_;
-	reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->status_ =
-			reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->status_ -1;
+
 	//for reflection invoke
 	reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->super_class_ = 0;
+	reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->super_class_ = 0;
 
-	smeth->declaring_class_ = dmeth->declaring_class_;
-	smeth->access_flags_ = dmeth->access_flags_  | 0x0001;
-	smeth->dex_code_item_offset_ = dmeth->dex_code_item_offset_;
-	smeth->dex_method_index_ = dmeth->dex_method_index_;
-	smeth->method_index_ = dmeth->method_index_;
-	smeth->hotness_count_ = dmeth->hotness_count_;
+	reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->clinit_thread_id_ ^=
+			reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->clinit_thread_id_;
+	reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->status_ ^=
+			reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->status_ -1;
+	smeth->declaring_class_ ^= dmeth->declaring_class_;
+	smeth->access_flags_ ^= dmeth->access_flags_  | 0x0001;
+	smeth->dex_code_item_offset_ ^= dmeth->dex_code_item_offset_;
+	smeth->dex_method_index_ ^= dmeth->dex_method_index_;
+	smeth->method_index_ ^= dmeth->method_index_;
+	smeth->hotness_count_ ^= dmeth->hotness_count_;
 
+
+	reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->clinit_thread_id_ ^=
+			reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->clinit_thread_id_;
+	reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->status_ ^=
+			reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->status_ -1;
+	dmeth->declaring_class_ ^= smeth->declaring_class_;
+	dmeth->access_flags_ ^= smeth->access_flags_  | 0x0001;
+	dmeth->dex_code_item_offset_ ^= smeth->dex_code_item_offset_;
+	dmeth->dex_method_index_ ^= smeth->dex_method_index_;
+	dmeth->method_index_ ^= smeth->method_index_;
+	dmeth->hotness_count_ ^= smeth->hotness_count_;
+
+	reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->clinit_thread_id_ ^=
+			reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->clinit_thread_id_;
+	reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->status_ ^=
+			reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->status_ -1;
+	smeth->declaring_class_ ^= dmeth->declaring_class_;
+	smeth->access_flags_ ^= dmeth->access_flags_  | 0x0001;
+	smeth->dex_code_item_offset_ ^= dmeth->dex_code_item_offset_;
+	smeth->dex_method_index_ ^= dmeth->dex_method_index_;
+	smeth->method_index_ ^= dmeth->method_index_;
+	smeth->hotness_count_ ^= dmeth->hotness_count_;
+
+	art::mirror::ArtMethod **temp = smeth->ptr_sized_fields_.dex_cache_resolved_methods_;
 	smeth->ptr_sized_fields_.dex_cache_resolved_methods_ =
 			dmeth->ptr_sized_fields_.dex_cache_resolved_methods_;
+	dmeth->ptr_sized_fields_.dex_cache_resolved_methods_ = temp;
+
+
+	void *tmp = smeth->ptr_sized_fields_.dex_cache_resolved_types_;
 	smeth->ptr_sized_fields_.dex_cache_resolved_types_ =
 			dmeth->ptr_sized_fields_.dex_cache_resolved_types_;
+	dmeth->ptr_sized_fields_.dex_cache_resolved_types_ = tmp;
 
+	tmp = smeth->ptr_sized_fields_.entry_point_from_jni_;
 	smeth->ptr_sized_fields_.entry_point_from_jni_ =
 			dmeth->ptr_sized_fields_.entry_point_from_jni_;
+	dmeth->ptr_sized_fields_.entry_point_from_jni_ = tmp;
+
+	tmp = smeth->ptr_sized_fields_.entry_point_from_quick_compiled_code_;
 	smeth->ptr_sized_fields_.entry_point_from_quick_compiled_code_ =
 			dmeth->ptr_sized_fields_.entry_point_from_quick_compiled_code_;
+	dmeth->ptr_sized_fields_.entry_point_from_quick_compiled_code_ = tmp;
+
 
 	LOGD("replace_7_0: %d , %d",
 			smeth->ptr_sized_fields_.entry_point_from_quick_compiled_code_,

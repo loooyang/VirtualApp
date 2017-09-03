1dc37779822cdf00abca5532c4d7a51c78e341ad
“smartdone” <smartdone@outlook.com>
2017-09-03 08:33:16 +0800

    android 4.4 swap function

--- andfixtester/app/src/main/jni/art/art_method_replace_4_4.cpp
+++ andfixtester/app/src/main/jni/art/art_method_replace_4_4.cpp
@@ -50,36 +50,97 @@ void replace_4_4(JNIEnv* env, jobject src, jobject dest) {
 	art::mirror::ArtMethod* dmeth =
 			(art::mirror::ArtMethod*) env->FromReflectedMethod(dest);
 
+    void *temp = dmeth->declaring_class_->class_loader_;
 	dmeth->declaring_class_->class_loader_ =
 			smeth->declaring_class_->class_loader_; //for plugin classloader
-	dmeth->declaring_class_->clinit_thread_id_ =
-			smeth->declaring_class_->clinit_thread_id_;
-	dmeth->declaring_class_->status_ = smeth->declaring_class_->status_-1;
+    smeth->declaring_class_->class_loader_ = temp;
+
 	//for reflection invoke
-	reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->super_class_ = 0;
+    reinterpret_cast<art::mirror::Class*>(dmeth->declaring_class_)->super_class_ = 0;
+    reinterpret_cast<art::mirror::Class*>(smeth->declaring_class_)->super_class_ = 0;
 
+    art::mirror::Class *ctemp = smeth->declaring_class_;
 	smeth->declaring_class_ = dmeth->declaring_class_;
+    dmeth->declaring_class_ = ctemp;
+
+    temp = smeth->dex_cache_initialized_static_storage_;
     smeth->dex_cache_initialized_static_storage_ = dmeth->dex_cache_initialized_static_storage_;
-    smeth->access_flags_ = dmeth->access_flags_  | 0x0001;
+    dmeth->dex_cache_initialized_static_storage_ = temp;
+
+    temp = smeth->dex_cache_resolved_types_;
     smeth->dex_cache_resolved_types_ = dmeth->dex_cache_resolved_types_;
+    dmeth->dex_cache_resolved_types_ = temp;
+
+    temp = smeth->dex_cache_resolved_methods_;
     smeth->dex_cache_resolved_methods_ = dmeth->dex_cache_resolved_methods_;
+    dmeth->dex_cache_resolved_methods_ = temp;
+
+    temp = smeth->dex_cache_strings_;
     smeth->dex_cache_strings_ = dmeth->dex_cache_strings_;
-    smeth->code_item_offset_ = dmeth->code_item_offset_;
-    smeth->core_spill_mask_ = dmeth->core_spill_mask_;
-    smeth->fp_spill_mask_ = dmeth->fp_spill_mask_;
-    smeth->method_dex_index_ = dmeth->method_dex_index_;
+    dmeth->dex_cache_strings_ = temp;
+
+    uint32_t *uitemp = (uint32_t *) smeth->mapping_table_;
     smeth->mapping_table_ = dmeth->mapping_table_;
-    smeth->method_index_ = dmeth->method_index_;
+    dmeth->mapping_table_ = uitemp;
+
+    uint8_t *u1temp = (uint8_t *) smeth->gc_map_;
     smeth->gc_map_ = dmeth->gc_map_;
-    smeth->frame_size_in_bytes_ = dmeth->frame_size_in_bytes_;
+    dmeth->gc_map_ = u1temp;
+
+    temp = (void *) smeth->native_method_;
     smeth->native_method_ = dmeth->native_method_;
-    smeth->vmap_table_ = dmeth->vmap_table_;
+    dmeth->native_method_ = temp;
 
+    temp = (void *) smeth->entry_point_from_compiled_code_;
     smeth->entry_point_from_compiled_code_ = dmeth->entry_point_from_compiled_code_;
+    dmeth->entry_point_from_compiled_code_ = temp;
 
+    temp = smeth->entry_point_from_interpreter_;
     smeth->entry_point_from_interpreter_ = dmeth->entry_point_from_interpreter_;
+    dmeth->entry_point_from_interpreter_ = temp;
 
-    smeth->method_index_ = dmeth->method_index_;
+    uint16_t *u2temp = (uint16_t *) smeth->vmap_table_;
+    smeth->vmap_table_ = dmeth->vmap_table_;
+    dmeth->vmap_table_ = u2temp;
+
+
+    smeth->method_index_ ^= dmeth->method_index_;
+    smeth->frame_size_in_bytes_ ^= dmeth->frame_size_in_bytes_;
+    smeth->method_index_ ^= dmeth->method_index_;
+    smeth->method_dex_index_ ^= dmeth->method_dex_index_;
+    smeth->fp_spill_mask_ ^= dmeth->fp_spill_mask_;
+    smeth->core_spill_mask_ ^= dmeth->core_spill_mask_;
+    smeth->code_item_offset_ ^= dmeth->code_item_offset_;
+    smeth->access_flags_ ^= dmeth->access_flags_  | 0x0001;
+    dmeth->declaring_class_->clinit_thread_id_ ^=
+            smeth->declaring_class_->clinit_thread_id_;
+    dmeth->declaring_class_->status_ ^= smeth->declaring_class_->status_-1;
+
+
+    dmeth->method_index_ ^= smeth->method_index_;
+    dmeth->frame_size_in_bytes_ ^= smeth->frame_size_in_bytes_;
+    dmeth->method_index_ ^= smeth->method_index_;
+    dmeth->method_dex_index_ ^= smeth->method_dex_index_;
+    dmeth->fp_spill_mask_ ^= smeth->fp_spill_mask_;
+    dmeth->core_spill_mask_ ^= smeth->core_spill_mask_;
+    dmeth->code_item_offset_ ^= smeth->code_item_offset_;
+    dmeth->access_flags_ ^= smeth->access_flags_  | 0x0001;
+    smeth->declaring_class_->clinit_thread_id_ ^=
+            dmeth->declaring_class_->clinit_thread_id_;
+    smeth->declaring_class_->status_ ^= dmeth->declaring_class_->status_-1;
+
+    smeth->method_index_ ^= dmeth->method_index_;
+    smeth->frame_size_in_bytes_ ^= dmeth->frame_size_in_bytes_;
+    smeth->method_index_ ^= dmeth->method_index_;
+    smeth->method_dex_index_ ^= dmeth->method_dex_index_;
+    smeth->fp_spill_mask_ ^= dmeth->fp_spill_mask_;
+    smeth->core_spill_mask_ ^= dmeth->core_spill_mask_;
+    smeth->code_item_offset_ ^= dmeth->code_item_offset_;
+    smeth->access_flags_ ^= dmeth->access_flags_  | 0x0001;
+    dmeth->declaring_class_->clinit_thread_id_ ^=
+            smeth->declaring_class_->clinit_thread_id_;
+    dmeth->declaring_class_->status_ ^= smeth->declaring_class_->status_-1;
+
 
 	LOGD("replace_4_4: %d , %d", smeth->entry_point_from_compiled_code_,
 			dmeth->entry_point_from_compiled_code_);
